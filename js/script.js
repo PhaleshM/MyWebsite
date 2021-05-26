f31d87c2c59fa35673b64c29d0dca4e452b54496
Phalesh <phaleshm@gmail.com>
2021-05-26 22:14:18 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,90 @@
+ // Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDkINTNRFvsWDE6di24IyGwoMgw5fsQWD0",
+    authDomain: "mywebsite-f0e1e.firebaseapp.com",
+    databaseURL: "https://mywebsite-f0e1e-default-rtdb.firebaseio.com",
+    projectId: "mywebsite-f0e1e",
+    storageBucket: "mywebsite-f0e1e.appspot.com",
+    messagingSenderId: "611803585661",
+    appId: "1:611803585661:web:947161df9c24cc44732514"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Reference Contact Information collection
+let contactInfo=firebase.database().ref("conInfo")
+
+let menu=document.querySelector(".menu")
+let meIt=document.querySelector(".meIt")
+
+let overmenu=document.querySelector(".overmenu")
+menu.addEventListener("click",overMenu)
+meIt.addEventListener("click",overMenu)
+
+function overMenu(){
+    let dspl=overmenu.style.display
+    if(dspl=="" || dspl=="none"){
+        overmenu.style.display="initial"
+    }else{
+        overmenu.style.display="none"
+    }
+}
+
+
+document.querySelector("#fs-frm").addEventListener("submit",collectInfo)
+// document.querySelector('#send').addEventListener("click",sendEmail)
+function collectInfo(e){
+    e.preventDefault()
+
+    var name=document.querySelector("#full-name").value
+    var email=document.querySelector("#email-address").value
+    var message=document.querySelector("#message").value
+    var hidden=document.querySelector("#hidden").value
+    console.log(name,email,message)
+    if(hidden.length==0){
+        saveContactInfo(name,email,message)
+//         sendEmail(name,email,message)        
+    }
+    document.querySelector("#fs-frm").reset()
+}
+
+function saveContactInfo(name,email,message){
+    let newContactInfo=contactInfo.push()
+    newContactInfo.set({
+        name:name,
+        email:email,
+        message:message
+    })
+}
+
+<<<<<<< HEAD
+function sendEmail(name,email,message){
+    Email.send({
+        // Host : "smtp.gmail.com",
+        // Username : "sphinkegypt@gmail.com",
+        // Password : "rdrauotfbpkntcwa",
+        SecureToken:'3e359ebc-302b-4749-abbf-b4100855ab94',
+        To : 'phaleshm@gmail.com',
+        From : email,
+        Subject : 'Mail from '+name,
+        Body : message
+    }).then(
+      message => alert(message)
+    );
+}
+=======
+// function sendEmail(name,email,message){
+//     Email.send({
+//         Host : "smtp.gmail.com",
+//         Username : "sphinkegypt@gmail.com",
+//         Password : "bujkastetcvsretq",
+//         // SecureToken:'4554691b-786d-4c4c-845d-80a6f10b615b',
+//         To : 'phaleshm@gmail.com',
+//         From : "sphinkegypt@gmail.com",
+//         Subject : "This is the subject",
+//         Body : "And this  is the body"
+//     }).then(
+//       message => alert(message)
+//     );
+// }
+>>>>>>> f343b709c8d3d802c5f3bc1510ad26beb7ed1bf4
